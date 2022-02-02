c71af7e48261211c44b55bff10435963b4d1c24a
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-02-02 22:40:36 +0300

    Home work 1

---
+++ src/p2-homeworks/h1/AlternativeMessage.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import s from "./Message.module.css";
+
+type AlternativeMessagePropsType = {
+    avatar: string;
+    name: string,
+    message: string,
+    time: string
+}
+
+function AlternativeMessage(props: AlternativeMessagePropsType) {
+    return (
+        <div className={s.message}>
+
+            <img src={props.avatar}
+                 className={s.avatar}
+                 alt={"image"}/>
+
+            <div className={s.angle}/>
+
+            <div className={s.content}>
+                <div className={s.name}>
+                    {props.name}</div>
+
+                <div className={s.text}>
+                    {props.message}</div>
+
+                <div className={s.time}>
+                    {props.time}</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AlternativeMessage
---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Message from "./Message";
+import AlternativeMessage from "./AlternativeMessage";
+
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
+
+const messageDataAlternative = {
+    avatar: 'http://st.gde-fon.com/wallpapers_original/621485_minenyi_studiya-piksar_multgeroi_7332x8299_www.Gde-Fon.com.jpg',
+    name: 'Mr. Smith',
+    message: 'Hello!!',
+    time: '22:01',
+}
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            should work (должно работать)
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            <AlternativeMessage
+                avatar={messageDataAlternative.avatar}
+                name={messageDataAlternative.name}
+                message={messageDataAlternative.message}
+                time={messageDataAlternative.time}
+            />
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
